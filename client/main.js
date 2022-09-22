c2007941767f945c55b9f2efb7dd6f691216f21e
huong.haduc <huong.ha@ntq-solution.com.vn>
2022-09-22 15:25:58 +0700

    update and remove

--- client/main.js
+++ client/main.js
@@ -1,7 +1,9 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
+import '../imports/startup/accounts-config.js';
+import '../imports/ui/body.js';
+import '../imports/api/tasks.js';
+// import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
@@ -20,3 +22,5 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
+
